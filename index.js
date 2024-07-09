5d025ea378b0b22d53ff4df851eefe9ba8945552
BusaidiEssa <116034947+BusaidiEssa@users.noreply.github.com>
2024-07-10 01:12:30 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+
+
+let countHome = 0
+let countGuest = 0
+let scoreHome =document.getElementById("score-h")
+let scoreGuest =document.getElementById("score-g")
+
+function add1h(){
+    countHome+=1
+    scoreHome.textContent = countHome
+    console.log(scoreHome)
+    
+}
+function add2h(){
+    countHome+=2
+    scoreHome.textContent = countHome
+    console.log(scoreHome)
+    
+}
+function add3h(){
+    countHome+=1
+    scoreHome.textContent = countHome
+    console.log(scoreHome)
+    
+}
+
+function add1g(){
+    countGuest+=1
+    scoreGuest.textContent = countGuest
+    console.log(scoreGuest)
+    
+}
+function add2g(){
+    countGuest+=2
+    scoreGuest.textContent = countGuest
+    console.log(scoreGuest)
+    
+}
+function add3g(){
+    countGuest+=1
+    scoreGuest.textContent = countGuest
+    console.log(scoreGuest)
+    
+}
